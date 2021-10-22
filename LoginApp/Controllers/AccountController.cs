196a188954ff1574836777760015273c703cdf87
hubert1217 <hubert.roj@gmail.com>
2021-10-22 10:44:20 +0200

    Prosty panel administracyjny skonczony

--- LoginApp/Controllers/AccountController.cs
+++ LoginApp/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LoginApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,34 @@ namespace LoginApp.Controllers
 {
     public class AccountController : Controller
     {
+        
+        [HttpGet]
         // GET: Account
-        public ActionResult Index()
+        public ViewResult Login()
         {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl) 
+        {
+            IAuthProvider authProvider = new FormsAuthProvider();
+
+            if (ModelState.IsValid)
+            {
+                if (authProvider.Authenticate(model.UserName, model.Password))
+                {
+                    return Redirect(returnUrl ?? Url.Action("Index", "Admin"));
+                }
+                else
+                {
+                    ModelState.AddModelError("", "Nieprawidłowa nazwa użytkownka lub nieproawidłowe hasło");
+                    return View();
+                }
+            }
+            else
+            {
+                return View();
+            }
         }
     }
 }
--- LoginApp/Models/LoginViewModel.cs
+++ LoginApp/Models/LoginViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,11 @@ namespace LoginApp.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage ="Proszę podać nazwę uzytkownika.")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage ="Prosze podac hasło.")]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
