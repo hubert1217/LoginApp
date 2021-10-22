e0f10b9e575ae5b4faf06532e9c8a64223b7835f
hubert1217 <hubert.roj@gmail.com>
2021-10-22 09:11:42 +0200

    Dodaj pliki projektów.

---
+++ LoginApp/Models/FormsAuthProvider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+
+namespace LoginApp.Models
+{
+    public class FormsAuthProvider : IAuthProvider
+    {
+        public bool Authenticate(string login, string pass)
+        {
+            bool result = FormsAuthentication.Authenticate(login, pass);
+
+            if (result) 
+            {
+                FormsAuthentication.SetAuthCookie(login, false);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LoginApp/Models/IAuthProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginApp.Models
+{
+    public interface IAuthProvider
+    {
+        bool Authenticate(string login, string pass);
+    }
+}
